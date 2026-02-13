5231a6ea110d1cf5361960d221e887afe211c53a
Yugant Singh <yugantsingh9651@gmail.com>
2026-02-13 12:35:14 +0530

    create post method -/api/tweet/

--- Backend/src/controllers/tweet.controller.js
+++ Backend/src/controllers/tweet.controller.js
@@ -1,5 +1,6 @@
 const tweetModel = require("../models/tweet.model")
 const Imagekit = require("imagekit")
+const jwt = require("jsonwebtoken")
 
 const imagekit = new Imagekit({
     privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
@@ -11,9 +12,36 @@ async function createTweetController(req, res) {
     const file = req.file
     const result = await imagekit.upload({
         file: file.buffer,
-        fileName: file.originalname
+        fileName: file.originalname,
+        folder:"Twitter/tweets"
+
     })
 
+    const token = req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message:"token not provided! unauthorized access"
+        })
+    }
+    let decode = null
+try{
+  decode = jwt.verify(token,process.env.JWT_SECRET)
+} catch(err){
+    return res.status(401).json({
+        message:"token invalid ! unauthorized access"
+    })
+}
+
+    const tweet = await tweetModel.create({
+        content:req.body.content,
+        imgUrl:result.url,
+        user:decode.id
+    })
+
+    res.status(201).json({
+        message:"tweet create successfully",
+        tweet
+    })
 
 }
 
--- Backend/src/routes/tweet.routes.js
+++ Backend/src/routes/tweet.routes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const tweetRouter = express.Router()
+
 const multer = require("multer")
 const upload = multer({storage:multer.memoryStorage()})
 const tweetController = require("../controllers/tweet.controller")
